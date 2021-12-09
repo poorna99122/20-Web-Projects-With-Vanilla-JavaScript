c70d7321349ebf8fadc48c639db692b60c04ac8a
poorna99122 <poornachandrarao99122@gmail.com>
2021-12-09 11:31:58 -0800

    final commit after all the validations in the form

--- FormValidator/script.js
+++ FormValidator/script.js
@@ -18,45 +18,122 @@ function showSuccess(input) {
   formControl.className = "form-control success";
 }
 
+
+
+
+
+
+
+
 //Checking emails is valid or not
-function isValidEmail(email) {
+function checkEmail(input) {
   const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return re.test(String(email).toLowerCase());
+  if(re.test(input.value.trim())){
+    showSuccess(input);
+
+  }
+  else {
+    showError(input,"Email is not valid");
+  }
 }
+
+//Check Required field
+function checkRequired(inputArr){
+inputArr.forEach( function(input) {
+// console.log(input.value );
+if(input.value.trim() === ''){
+showError(input, `${getFieldName(input)} is required`);
+}
+else{
+  showSuccess(input);
+}
+});
+}
+
+//checking input lengths 
+function checkLength (input,min,max){
+  if(input.value.length < min ){
+    showError(input,`${getFieldName(input)} must be atleast ${min} characters`);
+  }
+  else if(input.value.length > max ){
+    showError(input,`${getFieldName(input)}  not be greater than ${max} characters`);
+  }
+  else {
+    showSuccess(input);
+  }
+
+}
+
+
+//Checking passwords
+function checkPasswordsMatch(input1,input2){
+  if(input1.value !== input2.value){
+    showError(input2,"Passwords should match");
+  }
+
+}
+
+
+
+
+
+//getFieldName
+function getFieldName(input){
+// return input.id.toUpperCase();
+return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+
+}
+
+
+
+
+
+
+
+
+
 
 //This addEventListener is taking 2 things first is the event we want to listen for and a function
 //Event Listeners
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
+  checkRequired([userName,email,password,confirmPassword]);
+  checkLength(userName, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password,confirmPassword);
+ 
+
+
   //Validation for userName
-  if (userName.value === "") {
-    // alert('UserName is Required');
-    showError(userName, "Username is required");
-  } else {
-    showSuccess(userName);
-  }
+  // if (userName.value === "") {
+  //   // alert('UserName is Required');
+  //   showError(userName, "Username is required");
+  // } else {
+  //   showSuccess(userName);
+  // }
 
   //Validation for email
-  if (email.value === "") {
-    showError(email, "email is required");
-  } else if (!isValidEmail(email.value)) {
-    showError(email, "Please enter valid email");
-  } else {
-    showSuccess(email);
-  }
+  // if (email.value === "") {
+  //   showError(email, "email is required");
+  // } else if (!isValidEmail(email.value)) {
+  //   showError(email, "Please enter valid email");
+  // } else {
+  //   showSuccess(email);
+  // }
 
   //Validation for password
-  if (password.value === "") {
-    showError(password, "password is required");
-  } else {
-    showSuccess(password);
-  }
+  // if (password.value === "") {
+  //   showError(password, "password is required");
+  // } else {
+  //   showSuccess(password);
+  // }
 
   //Validation for confirmPassword
-  if (confirmPassword.value === "") {
-    showError(confirmPassword, "confirmPassword is required");
-  } else {
-    showSuccess(confirmPassword);
-  }
+  // if (confirmPassword.value === "") {
+  //   showError(confirmPassword, "confirmPassword is required");
+  // } else {
+  //   showSuccess(confirmPassword);
+  // }
 });
