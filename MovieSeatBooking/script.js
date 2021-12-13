e250e6ec8c7e43578e36ab22ce8decca046b3a26
poorna99122 <poornachandrarao99122@gmail.com>
2021-12-12 20:08:25 -0800

    Adding the selected seats into LocalStorage

--- MovieSeatBooking/script.js
+++ MovieSeatBooking/script.js
@@ -1,69 +1,75 @@
-//querySelector returns a single object with the first HTML element that matches the 'selectors'
 const container = document.querySelector('.container');
-console.log(container.value);
-
-
-
-//querySelectorAll() returns an array of objects with all the HTML elements that match the 'selectors'.
-const seats = document.querySelectorAll('.row.seats:not(.occupied)');
-console.log(seats.value);
-
-
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+// populateUI();
 
+let ticketPrice = +movieSelect.value;
 
-//Initial ticket price
-let ticketPrice = parseInt(movieSelect.value);
-console.log(typeof ticketPrice);
+// Save selected movie index and price
+// function setMovieData(movieIndex, moviePrice) {
+//   localStorage.setItem('selectedMovieIndex', movieIndex);
+//   localStorage.setItem('selectedMoviePrice', moviePrice);
+// }
 
+// Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+  // console.log(seatsIndex);
 
-function updateSelectedCount(){
-const selectedSeats = document.querySelectorAll('.row .seat.selected');
-// console.log(selectedSeats);
+  localStorage.setItem('selectedSeats',JSON.stringify(seatsIndex));
 
-const selectedSeatsCount = selectedSeats.length;
-console.log(selectedSeatsCount);
+  // localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
+  const selectedSeatsCount = selectedSeats.length;
 
-//setting no of seats to the innerText of the span tag with id count
-count.innerText = selectedSeatsCount;   
-total.innerText = selectedSeatsCount * ticketPrice;
-
-
-
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+  
+  // setMovieData(movieSelect.selectedIndex, movieSelect.value);
 }
 
+// Get data from localstorage and populate UI
+// function populateUI() {
+//   const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
 
+//   if (selectedSeats !== null && selectedSeats.length > 0) {
+//     seats.forEach((seat, index) => {
+//       if (selectedSeats.indexOf(index) > -1) {
+//         seat.classList.add('selected');
+//       }
+//     });
+//   }
 
-//Movie select event
-movieSelect.addEventListener('change', (e)=>{
-    ticketPrice = parseInt(e.target.value);
-    updateSelectedCount();
+//   const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
 
+//   if (selectedMovieIndex !== null) {
+//     movieSelect.selectedIndex = selectedMovieIndex;
+//   }
+// }
+
+// Movie select event
+movieSelect.addEventListener('change', e => {
+  ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
 });
 
+// Seat click event
+container.addEventListener('click', e => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
 
-
-
-//This is to toggle the class of seat to turn blue when selected if not to normal when we select again
-//Seat click event listener
-container.addEventListener('click', (e) => {
-    // console.log(e.target);
-    if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
-        // console.log(e.target);
-        e.target.classList.toggle('selected');
-    }
-
-
-    //To update the count when we select or deselect the seat
     updateSelectedCount();
-})
+  }
+});
 
-
-
-
-
+// Initial count and total set
+updateSelectedCount();
