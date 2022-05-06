6c551b68dcaa9fae00c086e9e99ff35aa5ec540f
poorna99122 <poornachandrarao99122@gmail.com>
2022-05-06 08:56:40 -0700

    we are looping the data and updating the DOM and adding event listeners

--- DOM_ArrayMethods/script.js
+++ DOM_ArrayMethods/script.js
@@ -11,24 +11,47 @@ getRandomUser();
 getRandomUser();
 getRandomUser();
 
-// Fetch random user and add money
+/* Fetch random user and add money */
 async function getRandomUser() {
   const res = await fetch("https://randomuser.me/api");
   const data = await res.json();
-
   const user = data.results[0];
-
+  
   const newUser = {
     name: `${user.name.first} ${user.name.last}`,
     money: Math.floor(Math.random() * 1000000),
   };
-
-  console.log(newUser);
-
   addData(newUser);
 }
 
-//Add new Obj to data Array
+/* Add new Obj to data Array */
 function addData(obj) {
   data.push(obj);
+  updateDOM();
 }
+
+
+/*Update DOM
+Here in updateDOM function we are not passing any arguments So, if we are not passing any arguments 
+Them it will take default as data array */
+function updateDOM(providedData = data){
+                //Clear the main div
+                main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+                providedData.forEach(item => {
+                    const element = document.createElement('div');
+                    element.classList.add('person');
+                    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+                    main.appendChild(element);
+                });
+}
+
+
+/* Format number as money */
+function formatMoney(number){
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+
+//Event Listeners
+addUserBtn.addEventListener('click',getRandomUser)
